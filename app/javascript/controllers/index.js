22466272078c2a44071588ba1051272aedfe71fb
biancapower <bianca@biancapower.com>
2022-07-09 21:04:35 +1000

    set up stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import DragController from "./drag_controller.js"
+application.register("drag", DragController)
