5639ec8c9b096966f71e1d896db95a7122698523
biancapower <bianca@biancapower.com>
2022-07-09 21:35:18 +1000

    drag and drop works but doesn't persist

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
 
 export default class extends Controller {
   connect() {
-    console.log("Connected!")
+    console.log("Connected!");
+    this.sortable = Sortable.create(this.element, {
+      onEnd: this.end.bind(this)
+    })
+  }
+
+  end(event) {
+    console.log(event)
   }
 }
